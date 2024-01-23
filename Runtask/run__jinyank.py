2ddcb81a74beb7d95c6e3a8bb96cfa539b011df4
dorian5656 <1069461929@qq.com>
2024-01-24 00:28:21 +0800

    金燕可以推送数据库

--- Runtask/run__jinyank.py
+++ Runtask/run__jinyank.py
@@ -477,7 +477,6 @@ class RunSxz(object):
     def update_mysql(self):
 
         from DataBaseInfo.MysqlInfo.MysqlTools import MysqlCurd
-        MC = MysqlCurd()
         from datetime import datetime
         # 获取当前时间
         current_time = datetime.now()
@@ -485,7 +484,6 @@ class RunSxz(object):
         end_run_time = current_time.strftime("%Y-%m-%d %H:%M:%S")
         update_sql_success = F"update   data_oms  set  是否已完成 =1 ,填报开始时间 = '{self.start_run_time}',填报结束时间 = '{end_run_time}' where   日期='{self.today_1}' and 电场名称='{self.wfname}'"
 
-        MC.update(update_sql_success)
         new_nanfang = F'../DataBaseInfo/MysqlInfo/new_nanfang.yml'
         NEWMC = MysqlCurd(new_nanfang)
         print(NEWMC.query_sql())
